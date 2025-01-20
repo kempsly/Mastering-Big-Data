591bb9b0565d3cc133d863d3a810fefc370df8fd
kempsly <110250434+kempsly@users.noreply.github.com>
2025-01-20 09:36:15 +0000

    Add files via upload

---
+++ Hadoop-Mapreduce/WordCountProject/src/WordCountReducerWithMin.java
@@ -0,0 +1,32 @@
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducerWithMin extends Reducer<Text, IntWritable,Text,IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+
+        int minCount=0;
+        Configuration config = context.getConfiguration();
+        try {
+            minCount = Integer.parseInt(config.get("reducer.minWordCount"));
+        } catch (Exception e){
+
+        }
+
+        int count = 0;
+        for (IntWritable value:values)
+        {
+            count += value.get();
+        }
+
+        if (count>=minCount) {
+
+            context.write(key, new IntWritable(count));
+        }
+    }
+}
